ee5063074c153fe0c64f80baa484c6f9bb326472
coffeemania68 <gmschool0245@gmail.com>
2025-01-12 12:47:40 +0900

    Update imageUrls.js

--- imageUrls.js
+++ imageUrls.js
@@ -1,14 +1,14 @@
 const imageUrls = {
   mainBg: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-bg.jpg",
   setThumbnails: {
-     set1: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-01.png",
+      set1: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-01.png",
       set2: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-02.png",
       set3: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-03.png",
       set4: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-04.png",
       set5: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-05.png",
       set6: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-06.png"
-    },
-   badgeImages: {
+  },
+    badgeImages: {
         "상식 천재": "https://blog.kakaocdn.net/dn/bAymPL/btsLFhWZnAq/jFQgneAkWRhVYvrEGKDYmk/img.jpg",
         "상식 마스터": "https://blog.kakaocdn.net/dn/meEa1/btsLFExCUzR/QBJXO6n2HuIWf3ppdKEA60/img.webp",
        "상식 루키": "https://blog.kakaocdn.net/dn/cLZls9/btsLFZ9jsjs/zK0nbtJ5LlaxfChjVOIZo0/img.jpg",
