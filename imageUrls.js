bbbaee85ef6e17a67468b5f2e3e3f85d66a62abc
coffeemania68 <gmschool0245@gmail.com>
2025-01-12 11:53:15 +0900

    Create imageUrls.js

---
+++ imageUrls.js
@@ -0,0 +1,19 @@
+const imageUrls = {
+  mainBg: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-bg.jpg",
+  setThumbnails: {
+     set1: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-01.png",
+      set2: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-02.png",
+      set3: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-03.png",
+      set4: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-04.png",
+      set5: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-05.png",
+      set6: "https://raw.githubusercontent.com/coffeemania68/fortune-2025/main/quiz-icon-06.png"
+    },
+   badgeImages: {
+        "상식 천재": "https://blog.kakaocdn.net/dn/bAymPL/btsLFhWZnAq/jFQgneAkWRhVYvrEGKDYmk/img.jpg",
+        "상식 마스터": "https://blog.kakaocdn.net/dn/meEa1/btsLFExCUzR/QBJXO6n2HuIWf3ppdKEA60/img.webp",
+       "상식 루키": "https://blog.kakaocdn.net/dn/cLZls9/btsLFZ9jsjs/zK0nbtJ5LlaxfChjVOIZo0/img.jpg",
+        "상식 뉴비": "https://blog.kakaocdn.net/dn/bbyl6h/btsLGdMPbmt/W9fsg5DKqfbNDG8k3ESYVK/img.jpg",
+        "아재 감성": "https://blog.kakaocdn.net/dn/dgzo1m/btsLHmoUquy/LKYBjnYvqyyXNuSAGbukck/img.png"
+   }
+};
+export default imageUrls;
